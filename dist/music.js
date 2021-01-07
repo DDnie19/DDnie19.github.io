c3e7feb620032a25620f526dc58064c28dce4333
d9 <d9@D9>
2021-01-07 10:49:39 +0800

    Site updated: 2021-01-07 10:49:39

---
+++ dist/music.js
@@ -0,0 +1,12 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	{
+        name: "海底",
+        artist: '~~',
+        url: 'http://music.163.com/song/media/outer/url?id=1426649237.mp3',
+        }
+     ]
+});
