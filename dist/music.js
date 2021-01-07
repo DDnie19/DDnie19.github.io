5e0042a27e6383adb8a672ec0f154fca626a48cd
d9 <d9@D9>
2021-01-07 11:05:59 +0800

    Site updated: 2021-01-07 11:05:59

--- dist/music.js
+++ dist/music.js
@@ -7,7 +7,7 @@ const ap = new APlayer({
         name: "遇见",
         artist: '~~',
         url: 'https://dnie9.com/music/yj.mp3',
-	cover: 'https://dnie9.com/images/yj.jpg'
+	cover: 'https://dnie9.com/images/yj.png'
         }
      ]
 });
